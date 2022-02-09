aa41f3fc90fa1237091b14f4c6e309bab0fb0081
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-09 14:16:54 +0100

    bozza prog mail

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// chiedere all'utente la mail
+// verificare che la mail sia nella lista per accedere
+// di conseguenza creare una lista mail
+// stampare un messaggio di avviso se l'utente può accedere
