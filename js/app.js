1cbe21fcd70e8e56940c37daab8888b3b66b8821
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-09 14:45:47 +0100

    chiedo mail

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 // chiedere all'utente la mail
+
+const mailInput = document.getElementById("user-mail");
+console.log(mailInput);
+
+const formButton = document.getElementById('cta-form');
+console.log(formButton);
+
+formButton.addEventListener('click', function() {
+
+    const userMail = mailInput.value;
+    console.log(userMail);
+})
+
+
 // verificare che la mail sia nella lista per accedere
 // di conseguenza creare una lista mail
 // stampare un messaggio di avviso se l'utente può accedere
